1e1ba8624f4ba1d8c313bda861a055e13a5f1a3f
asarheroic <130447250+asarheroic@users.noreply.github.com>
2025-06-26 04:16:50 +0530

    Update generate token function utils.js
    secure: true is required when using sameSite: "None" — this ensures it only works on HTTPS (which Render and Vercel provide).

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,12 +5,26 @@ const {sign} = require( "jsonwebtoken")
         expiresIn : "7d"
     })
 
-    res.cookie("jwt",token ,{
-        maxAge : 7*24*60*60*1000 ,// ms
-        httpOnly : true ,// cross site scripting attacks means cant access using js
-        sameSite : "Lax",//csrf only links
+    // res.cookie("jwt",token ,{
+    //     maxAge : 7*24*60*60*1000 ,// ms
+    //     httpOnly : true ,// cross site scripting attacks means cant access using js
+    //     sameSite : "Lax",//csrf only links
         
-    })
+    // })  working locally and will work on monorepo but when FE(vercel) and BE(render) it gives error to set cookies so 
+
+
+
+    // secure: true is required when using sameSite: "None" — this ensures it only works on HTTPS (which Render and Vercel provide).
+ 
+
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "None",  // ✅ Needed for cross-origin cookies (Render <-> Vercel)
+        secure: true       // ✅ Required when using sameSite: "None"
+      })
+
+
     return token
 
 }
